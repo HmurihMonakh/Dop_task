902a309c7c90f4a17bad1703ef09e7d925444432
Hmuriy Monakh <max_08931993@mail.ru>
2022-08-04 18:06:16 +0300

    task my

---
+++ task_!1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Добро пожаловать в игру ");
+
+Console.WriteLine("Камнь, ножницы, бумага? - \"выход ");// 
+string userChoise=Console.ReadLine();
+// А = камень
+// B = ножницы
+// С = бумага
+// A > B ---- камень-А > ножницы-B
+// C > A ---- бумага-С > камень-А
+// B > C ---- ножницы-B > бумага-С
+// 
+string A = "камень ";
+string B = "ножницы ";
+string C = "бумага ";
+
+string? game = Console.ReadLine();
+
+
+
+
+
